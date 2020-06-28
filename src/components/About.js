0e0732bf668c07f3b513db6d78400cdddb7e44e2
ajgreenwell <greenwan@bc.edu>
2020-06-28 18:36:57 -0500

    changing about background

--- src/components/About.js
+++ src/components/About.js
@@ -6,19 +6,20 @@ export default function renderAbout(about) {
 
     return `
     <section id="about" class="row">
-        <div class="col-2 left-pane">
-            <img src="${image}">
-        </div>
-        <div class="col-10 about-info">
-            <p id="about-title">
-                <strong>${title}</strong><br>
-                ${address}<br>
-                <a href="mailto:${email}">${email}</a> |
-                <a href="${resume}" target="_blank"><i class="far fa-file-alt"></i> Resume</a> |
-                <a href="${linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a> |
-                <a href="${github}" target="_blank"><i class="fab fa-github"></i></a>
-            </p>
-            <p>${description}</p>
+        <img src="assets/background.png" alt="background-image" id="about-background"/>
+        <div id="about-content">
+            <div class="col-2 left-pane">
+                <img src="${image}" id="about-profile">
+            </div>
+            <div id="about-info" class="col-10">
+                <p id="about-title">
+                    <strong>${title}</strong><br>
+                    ${address}<br>
+                    <a href="mailto:${email}">${email}</a> |
+                    <a href="${resume}" target="_blank"><i class="far fa-file-alt"></i> Resume</a>
+                </p>
+                <p>${description}</p>
+            </div>
         </div>
     </section>
     `;
